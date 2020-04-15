f149dab7af2c3f85f120a50816946c8852cfe90d
Gabarros <gabarros17@gmail.com>
2020-04-15 16:34:13 -0300

    Create new components

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: TransferencesList(),
         appBar: AppBar(
           title: Text(
             'Transferências',
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('123456'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('123456'),
-              ),
-            ),
-          ],
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
         ),
-        floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
       ),
     ));
+
+class TransferencesList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferenceItem(Transference(300, 123455)),
+        TransferenceItem(Transference(500, 1387713)),
+        TransferenceItem(Transference(800, 145678)),
+      ],
+    );
+  }
+}
+
+class TransferenceItem extends StatelessWidget {
+
+  final Transference _transference;
+
+  TransferenceItem(this._transference);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transference.value.toString()),
+        subtitle: Text(_transference.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transference{
+  final double value;
+  final int accountNumber;
+
+  Transference(this.value, this.accountNumber);
+}
