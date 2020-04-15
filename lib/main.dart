7c391cc8a8d8d5af1a0d327aad03862988621369
Gabarros <gabarros17@gmail.com>
2020-04-15 16:14:02 -0300

    create widgets from the main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Transferências'),),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add)
+        appBar: AppBar(
+          title: Text(
+            'Transferências',
+          ),
         ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('123456'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('123456'),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
       ),
     ));
